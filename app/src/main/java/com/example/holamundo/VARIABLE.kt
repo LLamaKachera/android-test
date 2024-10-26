971d7117a5f438f2ddb47dbfc412cd54a07782b9
LLamaKachera <yamil.nav@outlook.com>
2024-10-26 17:21:19 -0400

    se añade cambios

---
+++ app/src/main/java/com/example/holamundo/VARIABLE.kt
@@ -0,0 +1,46 @@
+package com.example.holamundo
+//VARIABLES
+fun main(){
+
+    /** val es una constante; y var es una varaible
+     * VARIABLES NUMERICAS
+     * **/
+
+    //variables numericas
+    //INT -2.147.483.647 A 2.147.483.647
+    val age:Int = 30
+
+    //LONG -9.223.372.036.854.775.807 a 9.223.372.036.854.775.807
+    val example:Long = 30
+
+    //usar camelCase para las variables
+    //Float
+    val floatExampleNumberTwoYes:Float =30.5f
+
+    //Double, soportan 14 decimales
+    val doubleExample:Double = 34.4561236
+
+    /**
+     * VARIABLES ALFANUMERICAS
+     * **/
+
+    //Char - solo soporta solo un(1) caracter
+    var charExample1:Char = 'e'
+    var charExample2:Char = '2'
+    var charExample3:Char = '&'
+
+    //String
+    val stringExample:String= "Comida COmida Comida"
+
+    /**
+     * VARIABLES BOOLEANAS
+     * **/
+    val booleanExample:Boolean = true
+    val booleanExample2:Boolean = false
+
+    //salto de linea
+    //print("\n")
+
+    //imprimiendo un mensajes
+    //print("Holiwi")
+}
